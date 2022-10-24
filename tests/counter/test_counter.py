61b9df66fdbd8aa0884558b93dab94beb627f418
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-10-24 00:10:05 -0300

    feat: test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,3 +3,9 @@ from src.counter import count_ocurrences
 
 def test_counter():
     assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "Java") == 676
+    assert count_ocurrences("src/jobs.csv", "JavaScript") == 122
+    assert count_ocurrences("src/jobs.csv", "C++") == 236
+    assert count_ocurrences("src/jobs.csv", "PHP") == 36
+    assert count_ocurrences("src/jobs.csv", "R") == 544538
+    assert count_ocurrences("src/jobs.csv", "C#") == 57
