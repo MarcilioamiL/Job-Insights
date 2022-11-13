a324ea759a1c29144e9a3e51a4435b25d9ae8ff2
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-13 01:09:03 -0300

    feat: test_brazilian_jobs

---
+++ tests/brazilian/test_brazilian_jobs.py
@@ -0,0 +1,6 @@
+from src.brazilian_jobs import read_brazilian_file
+
+
+def test_brazilian_jobs():
+    for job in read_brazilian_file("tests/mocks/brazilians_jobs.csv"):
+        assert {"title", "salary", "type"} == job.keys()
